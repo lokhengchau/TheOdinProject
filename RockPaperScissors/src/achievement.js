a24972e706c2585a137ae709df94210364fb2b25
lokhchau <lokhengchau@gmail.com>
2024-03-21 22:23:21 -0700

    refactor achievement activate functions

--- RockPaperScissors/src/achievement.js
+++ RockPaperScissors/src/achievement.js
@@ -5,44 +5,25 @@ class Achievement {
     this.achieved = false;
     this.activate = fn;
   }
-}
 
-function displayAchievement(achievement) {
-  const achievementPanelDiv = document.querySelector('.achievement-wrapper');
-  const achievementDiv = document.createElement('div');
-  achievementDiv.classList = 'achievement';
-
-  const titleH2 = document.createElement('h2');
-  titleH2.innerText = achievement.title;
-  const descriptionP = document.createElement('p');
-  descriptionP.innerText = achievement.description;
-
-  achievementDiv.append(titleH2, descriptionP);
-  achievementPanelDiv.append(achievementDiv);
-  setTimeout(() => {
-    achievementDiv.remove();
-  }, 3000);
+  done() {
+    this.achieved = true;
+  }
 }
 
 function firstWin(playerRecord) {
-  if (playerRecord.score === 1) {
-    this.achieved = true;
-    displayAchievement(this);
-  }
+  if (playerRecord.score === 1) return true;
+  return false;
 }
 
 function firstLoss(playerRecord) {
-  if (playerRecord.loss === 1) {
-    this.achieved = true;
-    displayAchievement(this);
-  }
+  if (playerRecord.loss === 1) return true;
+  return false;
 }
 
 function firstTie(playerRecord) {
-  if (playerRecord.tie === 1) {
-    this.achieved = true;
-    displayAchievement(this);
-  }
+  if (playerRecord.tie === 1) return true;
+  return false;
 }
 
 const achievementArray = [
@@ -63,10 +44,34 @@ const achievementArray = [
   ),
 ];
 
+function displayAchievement(achievement) {
+  const achievementPanelDiv = document.querySelector('.achievement-wrapper');
+  const achievementDiv = document.createElement('div');
+  achievementDiv.classList = 'achievement';
+
+  const titleH2 = document.createElement('h2');
+  titleH2.innerText = achievement.title;
+  const descriptionP = document.createElement('p');
+  descriptionP.innerText = achievement.description;
+
+  achievementDiv.append(titleH2, descriptionP);
+  achievementPanelDiv.append(achievementDiv);
+
+  function animatinoEndFn() {
+    achievementDiv.removeEventListener('animationend', animatinoEndFn);
+    achievementDiv.remove();
+  }
+
+  achievementDiv.addEventListener('animationend', animatinoEndFn);
+}
+
 function checkAchievements(playerRecord) {
   achievementArray.forEach((achievement) => {
     if (achievement.achieved === false) {
-      achievement.activate(playerRecord);
+      if (achievement.activate(playerRecord)) {
+        achievement.done();
+        displayAchievement(achievement);
+      }
     }
   });
 }
